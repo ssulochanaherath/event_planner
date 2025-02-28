e60c1166398bb336e48fe42e93e3e4f159218edc
ssulochanaherath <ssulochanaherath.com>
2025-02-28 18:02:19 +0530

    tickets

---
+++ app/dashboard/ticket.tsx
@@ -0,0 +1,11 @@
+// app/dashboard/ticket.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function TicketBookingScreen() {
+  return (
+    <View>
+      <Text>Ticket Booking Screen</Text>
+    </View>
+  );
+}
